3c515435782c0fde7792d1eb2b26a0b904a641e9
geoffreyTools <geoffreyTools@users.noreply.github.com>
2024-02-18 15:21:04 +0100

    test: silence Jest errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Website has Little lemon logo with alt text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logos = screen.getAllByAltText(/Little Lemon Restaurant/i);
+  expect(logos[0]).toBeInTheDocument();
 });

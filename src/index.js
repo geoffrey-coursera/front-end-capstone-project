7f615d69b8783a3d4da93c29f24e9286392259b0
geoffreyTools <geoffreyTools@users.noreply.github.com>
2024-02-18 15:21:05 +0100

    feat: add shading colors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
